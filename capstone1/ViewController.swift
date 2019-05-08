275fa668486960ec5bfd25b06b6fc023c3b67e7e
gktmddlr <gktmddlr@gmail.com>
2019-05-08 15:33:55 +0900

    =wwwww

--- capstone1/ViewController.swift
+++ capstone1/ViewController.swift
@@ -8,13 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
+    
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var stockGraphImage: UIButton!
+   
+    let mainChart : UIImage = UIImage(named: "mainchart")!
+  
+    
+    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+                "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+                "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+                "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+                "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+    
+    var filteredData: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        searchBar.delegate = self
+        filteredData = data
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("1")
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("2")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as UITableViewCell
+        print("3")
+        cell.textLabel?.text = filteredData[indexPath.row]
+
+        return cell
+    }
+    
+   func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+
+        filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+            }
+    
+        tableView.reloadData()
+    }
 
 }
 
